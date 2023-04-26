3e4ba5c54a55241f1f86e4b316141eb726734f11
NancyMakram <128957636+NancyMakram@users.noreply.github.com>
2023-04-26 16:13:09 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
 
-int main(void) {
-  printf("_putchar\n");
-  return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
-
