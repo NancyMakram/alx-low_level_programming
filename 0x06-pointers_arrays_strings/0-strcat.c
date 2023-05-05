ff24c773e26b8641b7bfa4a2c09bc734e555a37e
NancyMakram <nancymakramaziz90@gmail.com>
2023-05-05 01:14:28 -0700

    main.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,54 +1,26 @@
-cat 0-main.c
 #include "main.h"
-#include <stdio.h>
-  /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string.
- *
- * Return: A pointer to the resulting string dest.
+/**
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
  */
 char *_strcat(char *dest, char *src)
 {
-    int dest_len = 0, i;
+	int count = 0, count2 = 0;
 
-    /* Find the length of dest string */
-    while (*(dest + dest_len))
-        dest_len++;
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-    /* Append the src string to the dest string */
-    for (i = 0; *(src + i); i++)
-        *(dest + dest_len + i) = *(src + i);
-
-    /* Null terminate dest string */
-    *(dest + dest_len + i) = '\0';
-
-    return dest;
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
-
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
-}
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-strcat.c -o 0-strcat
-  ./0-strcat
- Hello 
-World!
-Hello World!
-World!
-Hello World!
-  
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,27 @@
+cat 1-main.c
 #include "main.h"
+#include <stdio>
 
 /**
- * _strncat - Concatenates two strings using at most
- *            an inputted number of bytes from src.
- * @dest: The string to be appended upon.
- * @src: The string to be appended to dest.
- * @n: The number of bytes from src to be appended to dest.
+ * main - check the code
  *
- * Return: A pointer to the resulting string dest.
+ * Return: Always 0.
  */
-char *_strncat(char *dest, char *src, int n)
+int main(void)
 {
-	int index = 0, dest_len = 0;
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
