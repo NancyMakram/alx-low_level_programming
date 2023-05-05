418544dda411ed3da9630927b7a3e64e02763dc3
NancyMakram <nancymakramaziz90@gmail.com>
2023-05-05 00:57:43 -0700

    0-main.c

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,24 @@
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+    int dest_len = 0, i;
+
+    /* Find the length of dest string */
+    while (*(dest + dest_len))
+        dest_len++;
+
+    /* Append the src string to the dest string */
+    for (i = 0; *(src + i); i++)
+        *(dest + dest_len + i) = *(src + i);
+
+    /* Null terminate dest string */
+    *(dest + dest_len + i) = '\0';
+
+    return dest;
+}
